3ad8170fe40466ccb1d789a2c59f6e6164691e8c
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2021-11-21 12:09:30 +0700

    update converter ro TYPE2 to TYP1

--- components/forms/TransactionFormAny.js
+++ components/forms/TransactionFormAny.js
@@ -10,7 +10,7 @@ import TextAreaInput from "../../components/inputs/TextArea";
 import StackableContainer from "../layout/StackableContainer";
 
 class TransactionFormAny extends React.Component {
-constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -30,9 +30,39 @@ constructor(props) {
     });
   };
 
+  convertCLITransaction = (tx_json_parsed) => {
+    let msgValue = {}
+    let msg = tx_json_parsed.body.messages[0]
+    msgValue["type"] = msg["@type"]
+    msgValue["value"] = {}
+    for(const key in msg){
+      if(key === "type") continue;
+      msgValue["value"][key] = msg[key];
+    }
+
+    let fee = tx_json_parsed.auth_info.fee;
+    msgValue["fee"] = {}
+    msgValue["fee"]["gas"] = fee.gas_limit;
+    msgValue["fee"]["amount"] = fee.amount;
+
+    return msgValue;
+  }
+
   createTransaction = () => { 
-    let tx_json_pasred = JSON.parse(this.state.tx);
-    var msgValue = tx_json_pasred.msgs[0].value;    
+    let tx_json_parsed = JSON.parse(this.state.tx);
+    var msgValue;
+    var type;
+    var fee;
+    if("msgs" in tx_json_parsed){
+      msgValue = tx_json_parsed.msgs[0].value;
+      type = tx_json_parsed.msgs[0].type; 
+      fee = tx_json_parsed.fee
+    }else{
+      let msg = this.convertCLITransaction(tx_json_parsed);
+      msgValue = msg.value;
+      type = msg.type;
+      fee = msg.fee;
+    }
 
     console.log(msgValue)
 
@@ -43,50 +73,43 @@ constructor(props) {
 
     console.log(msgValue)
 
-    switch (tx_json_pasred.msgs[0].type) {
-      case 'cosmos-sdk/MsgWithdrawDelegationReward':
-        var type = '/cosmos.staking.v1beta1.MsgWithdrawDelegationReward';
-        msgValue.validatorAddress = msgValue.validator_address;
+    if(type === 'cosmos-sdk/MsgWithdrawDelegationReward' || type === '/cosmos.staking.v1beta1.MsgWithdrawDelegationReward'){
+      type = '/cosmos.staking.v1beta1.MsgWithdrawDelegationReward';
+      msgValue.validatorAddress = msgValue.validator_address;
 
-        delete msgValue.validator_address;
-        break;
-
-      case 'cosmos-sdk/MsgDelegate':
-        type = '/cosmos.staking.v1beta1.MsgDelegate';
-        msgValue.validatorAddress = msgValue.validator_address;
-          
-        delete msgValue.validator_address;
-        break;
-
-      case 'cosmos-sdk/MsgSend':
-        type = '/cosmos.bank.v1beta1.MsgSend'
-        break;
-
-      case 'cosmos-sdk/MsgUndelegate':
-        type = '/cosmos.staking.v1beta1.MsgUndelegate'
-        msgValue.validatorAddress = msgValue.validator_address;
-
-        delete msgValue.validator_address;
-        break;
+      delete msgValue.validator_address;
+    }
+    else if(type === 'cosmos-sdk/MsgDelegate' || type === '/cosmos.staking.v1beta1.MsgDelegate'){
+      type = '/cosmos.staking.v1beta1.MsgDelegate';
+      msgValue.validatorAddress = msgValue.validator_address;
         
-      case 'cosmos-sdk/MsgBeginRedelegate':
-        type = '/cosmos.staking.v1beta1.MsgBeginRedelegate'
-        msgValue.validatorAddress = msgValue.validator_address;
-        msgValue.validatorSrcAddress = msgValue.validator_src_address
-        msgValue.validatorDstAddress = msgValue.validator_dst_address
+      delete msgValue.validator_address;
+    }
+    else if(type === 'cosmos-sdk/MsgSend' || type === '/cosmos.bank.v1beta1.MsgSend'){
+      type = '/cosmos.bank.v1beta1.MsgSend'
+    } 
+    else if(type === 'cosmos-sdk/MsgUndelegate' || type === '/cosmos.staking.v1beta1.MsgUndelegate'){
+      type = '/cosmos.staking.v1beta1.MsgUndelegate'
+      msgValue.validatorAddress = msgValue.validator_address;
 
-        delete msgValue.validator_src_address, msgValue.validator_dst_address, msgValue.validator_address
-        break;
+      delete msgValue.validator_address;
+    }
+    else if(type === 'cosmos-sdk/MsgBeginRedelegate' || type === '/cosmos.staking.v1beta1.MsgBeginRedelegate'){
+      type = '/cosmos.staking.v1beta1.MsgBeginRedelegate'
+      msgValue.validatorAddress = msgValue.validator_address;
+      msgValue.validatorSrcAddress = msgValue.validator_src_address
+      msgValue.validatorDstAddress = msgValue.validator_dst_address
 
-      default: this.setState({ addressError: "Use a valid transaction" });
+      delete msgValue.validator_src_address, msgValue.validator_dst_address, msgValue.validator_address
+    }
+    else {
+      this.setState({ addressError: "Use a valid transaction" });
     }
 
     const msg = {
       value: msgValue,
       typeUrl : type,
     };
-
-    const fee = tx_json_pasred.fee
 
     return {
       accountNumber: this.props.accountOnChain.accountNumber,
@@ -155,138 +178,5 @@ constructor(props) {
     );
   }
 }
-// class TransactionFormAny extends React.Component {
-//     constructor(props) {
-//       super(props);
-  
-//       this.state = {
-//         toAddress: "",
-//         amount: 0,
-//         memo: "",
-//         gas: 200000,
-//         processing: false,
-//         addressError: "",
-//       };
-//     }
-  
-//     handleChange = (e) => {
-//       this.setState({
-//         [e.target.name]: e.target.value,
-//       });
-//     };
-  
-//     createTransaction = (toAddress, amount, gas) => {
-//       const msgSend = {
-//         fromAddress: this.props.address,
-//         toAddress: toAddress,
-//         amount: coins(amount * 1000000, process.env.NEXT_PUBLIC_DENOM),
-//       };
-//       const msg = {
-//         typeUrl: "/cosmos.bank.v1beta1.MsgSend",
-//         value: msgSend,
-//       };
-//       const gasLimit = gas;
-//       const fee = {
-//         amount: coins(6000, process.env.NEXT_PUBLIC_DENOM),
-//         gas: gasLimit.toString(),
-//       };
-  
-//       return {
-//         accountNumber: this.props.accountOnChain.accountNumber,
-//         sequence: this.props.accountOnChain.sequence,
-//         chainId: process.env.NEXT_PUBLIC_CHAIN_ID,
-//         msgs: [msg],
-//         fee: fee,
-//         memo: this.state.memo,
-//       };
-//     };
-  
-//     handleCreate = async () => {
-//       if (this.state.toAddress.length === 43) {
-//         this.setState({ processing: true });
-//         const tx = this.createTransaction(
-//           this.state.toAddress,
-//           this.state.amount,
-//           this.state.gas
-//         );
-//         console.log(tx);
-//         const dataJSON = JSON.stringify(tx);
-//         const res = await axios.post("/api/transaction", { dataJSON });
-//         const { transactionID } = res.data;
-//         this.props.router.push(
-//           `${this.props.address}/transaction/${transactionID}`
-//         );
-//       } else {
-//         this.setState({ addressError: "Use a valid osmosis address" });
-//       }
-//     };
-  
-//     render() {
-//       return (
-//         <StackableContainer lessPadding>
-//           <button className="remove" onClick={this.props.closeForm}>
-//             ✕
-//           </button>
-//           <h2>Create New transaction</h2>
-//           <div className="form-item">
-//             <Input
-//               label="To Address"
-//               name="toAddress"
-//               value={this.state.toAddress}
-//               onChange={this.handleChange}
-//               error={this.state.addressError}
-//               placeholder="osmo1ya403hmh5ehj2qp6uf0pa672ynjguc7aea4mpk"
-//             />
-//           </div>
-//           <div className="form-item">
-//             <Input
-//               label="Amount (OSMO)"
-//               name="amount"
-//               type="number"
-//               value={this.state.amount}
-//               onChange={this.handleChange}
-//             />
-//           </div>
-//           <div className="form-item">
-//             <Input
-//               label="Gas Limit (UOSMO)"
-//               name="gas"
-//               type="number"
-//               value={this.state.gas}
-//               onChange={this.handleChange}
-//             />
-//           </div>
-//           <div className="form-item">
-//             <Input
-//               label="Memo"
-//               name="memo"
-//               type="text"
-//               value={this.state.memo}
-//               onChange={this.handleChange}
-//             />
-//           </div>
-//           <Button label="Create Transaction" onClick={this.handleCreate} />
-//           <style jsx>{`
-//             p {
-//               margin-top: 15px;
-//             }
-//             .form-item {
-//               margin-top: 1.5em;
-//             }
-//             button.remove {
-//               background: rgba(255, 255, 255, 0.2);
-//               width: 30px;
-//               height: 30px;
-//               border-radius: 50%;
-//               border: none;
-//               color: white;
-//               position: absolute;
-//               right: 10px;
-//               top: 10px;
-//             }
-//           `}</style>
-//         </StackableContainer>
-//       );
-//     }
-// }
+
 export default withRouter(TransactionFormAny);
