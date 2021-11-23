2682bd3d49b0c11cf869b90b8fcdf2b4709d8235
vuong <nguyenvuong1122000@gmail.com>
2021-11-23 20:25:31 +0700

    fix logic bug in create multisig

--- pages/api/multisig/index.js
+++ pages/api/multisig/index.js
@@ -1,4 +1,4 @@
-import { createMultisig } from "../../../lib/graphqlHelpers";
+import { createMultisig, getMultisig } from "../../../lib/graphqlHelpers";
 
 export default async function (req, res) {
   return new Promise(async (resolve) => {
@@ -16,6 +16,19 @@ export default async function (req, res) {
           res.status(400).send(err.message);
           return resolve();
         }
+      case "GET" :
+        try {
+          console.log("Function `getMultisig` invoked", req.query.address_bech32);
+          const saveRes = await getMultisig(req.query.address_bech32);
+          console.log("success", saveRes.data);
+          res.status(200).send(saveRes.data);
+          return resolve();
+        } catch (err) {
+          console.log(err);
+          res.status(400).send(err.message);
+          return resolve();
+        }
+
     }
     // no route matched
     res.status(405).end();
